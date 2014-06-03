7c6addf035f4b74706f624512ac7bf75d5c8f573
藤原 尚樹 <f.naoki.226@gmail.com>
2014-06-03 23:41:07 +0900

    javascript コードきれいに

--- js/common.js
+++ js/common.js
@@ -1,219 +1,129 @@
 
+//ブラウザ判定参考
+var isWin9X = (navigator.appVersion.toLowerCase().indexOf('windows 98')+1);
+var isIE = (navigator.appName.toLowerCase().indexOf('internet explorer')+1?1:0);
+var isOpera = (navigator.userAgent.toLowerCase().indexOf('opera')+1?1:0);
+if (isOpera) isIE = false;
+var isSafari = (navigator.appVersion.toLowerCase().indexOf('safari')+1?1:0);
+
 
 //スクロール取得オブジェクト
+function getScrollPosition() {
+	var obj = new Object();
+	obj.x = document.documentElement.scrollLeft || document.body.scrollLeft;
+	obj.y = document.documentElement.scrollTop || document.body.scrollTop;
+	return obj;
+}
 
 
-    var isWin9X = (navigator.appVersion.toLowerCase().indexOf('windows 98')+1);
-    var isIE = (navigator.appName.toLowerCase().indexOf('internet explorer')+1?1:0);
-    var isOpera = (navigator.userAgent.toLowerCase().indexOf('opera')+1?1:0);
-    if (isOpera) isIE = false;
-    var isSafari = (navigator.appVersion.toLowerCase().indexOf('safari')+1?1:0);
-
-
-    function getScrollPosition() {
-    var obj = new Object();
-    obj.x = document.documentElement.scrollLeft || document.body.scrollLeft;
-    obj.y = document.documentElement.scrollTop || document.body.scrollTop;
-    return obj;
-    }
-
-
-    function getScreenSize() {
-    var obj = new Object();
-    if (!isSafari && !isOpera) {
-    obj.x = document.documentElement.clientWidth || document.body.clientWidth || document.body.scrollWidth;
-    obj.y = document.documentElement.clientHeight || document.body.clientHeight || document.body.scrollHeight;
-    } else {
-    obj.x = window.innerWidth;
-    obj.y = window.innerHeight;
-    }
-    obj.mx = parseInt((obj.x)/2);
-    obj.my = parseInt((obj.y)/2);
-    return obj;
-    }
-
-    function getDocumentSize(){
-    	var obj=new Object();
-    	obj.h = Math.max.apply( null, [document.body.clientHeight , document.body.scrollHeight, document.documentElement.scrollHeight, document.documentElement.clientHeight] );
-    	return obj;
-    }
-
-    //オブジェクトの相対位置取得
-    //右バナースクロール固定
-    function getobj_position($target){
-    	var obj=new Object();
-    	obj.x=$target.offset().left;
-    	obj.y=$target.offset().top;
-    	obj.h=$target.outerHeight();
-    	obj.w=$target.outerWidth();
-    	return obj;
-    }
-
-    function posi_fix($target,ypos){
-		console.log($target.css('position'))
-		if($target.css('position')!='fixed'){
-			$target.css('position','fixed');
-			$target.css('top',ypos);
-		}
+//スクリーンサイズ取得オブジェクト
+function getScreenSize() {
+	var obj = new Object();
+	if (!isSafari && !isOpera) {
+		obj.x = document.documentElement.clientWidth || document.body.clientWidth || document.body.scrollWidth;
+		obj.y = document.documentElement.clientHeight || document.body.clientHeight || document.body.scrollHeight;
+	} else {
+		obj.x = window.innerWidth;
+		obj.y = window.innerHeight;
 	}
-	function posi_relative($target,ypos){
-		//if($target.css('position')!='relative'){
-			$target.css('position','relative');
-			$target.css('top',ypos);
-		//}
-	}
-	//右バナースクロール株固定
+	obj.mx = parseInt((obj.x)/2);
+	obj.my = parseInt((obj.y)/2);
+	return obj;
+}
 
 
+//ドキュメントサイズ取得オブジェクト
+function getDocumentSize(){
+	var obj=new Object();
+	obj.h = Math.max.apply( null, [document.body.clientHeight , document.body.scrollHeight, document.documentElement.scrollHeight, document.documentElement.clientHeight] );
+	return obj;
+}
+
+//オブジェクトの相対位置取得オブジェクト
+function getobj_position($target){
+    var obj=new Object();
+    obj.x=$target.offset().left;
+    obj.y=$target.offset().top;
+    obj.h=$target.outerHeight();
+    obj.w=$target.outerWidth();
+    return obj;
+}
+
+
+function posi_fix($target,ypos){
+   	console.log($target.css('position'))
+   	if($target.css('position')!='fixed'){
+   		$target.css('position','fixed');
+   		$target.css('top',ypos);
+   	}
+}
+function posi_relative($target,ypos){
+	$target.css('position','relative');
+	$target.css('top',ypos);
+}
 
 
 
 //
 
 $(function(){
-
-
-
-
-	/*トップ基準で固定
-	function posi_fix($target,ypos){
-		console.log($target.css('position'))
-		if($target.css('position')!='fixed'){
-			$target.css('position','fixed');
-			$target.css('top',ypos);
-		}
-	}
-	function posi_abs($target,ypos){
-		if($target.css('position')!='absolute'){
-			$target.css('position','absolute');
-			$target.css('top',ypos);
-		}
-	}
-	トップ基準で固定*/
-
-
-
-	//scroll取得
-	/*ページ上部固定
-	$(window).on('scroll',function(){
-		var $base=$('#subcontent');
-		var $target=$('#banner_group');
-		var $footer=$('#footer');
-		var under_y=getobj_position($footer).y;
-		var s_obj=getScrollPosition();
-
-		var moto_obj=getobj_position($base);
-		var target_obj=getobj_position($target);
-		var sotai_x=moto_obj.x-target_obj.x;
-		var sotai_y=moto_obj.y-target_obj.y;
-
-		if(moto_obj.y<=s_obj.y+10){
-			console.log('on:'+-moto_obj.y);
-			console.log("幅："+target_obj.w+":高さ"+target_obj.h)
-			posi_fix($target,10);
-		}else {
-			posi_abs($target,0);
-		}
-
-		if(s_obj.y>=under_y-target_obj.h-60){
-			posi_abs($target,under_y-target_obj.h-moto_obj.y-60);
-		}
-
-		console.log(sotai_x+":"+sotai_y+"スクロール"+s_obj.y+"underY"+under_y);
-	})
-	ページ上部固定*/
-
-	//ページ下部固定
-
 	banner_move();
 	$(window).on('scroll',banner_move)
-
-
-	
-
-
 })
 
 
 //ページ下部固定
-	function banner_move(){
-		var $base=$('#sub_content');
-		var $target=$('#banner_group');
-		var $footer=$('#footer');
-		var under_y=getobj_position($footer).y;
-		var s_obj=getScrollPosition();
-		var d_obj=getDocumentSize();
-		//window
-		var winObj=getScreenSize();
-
-		var moto_obj=getobj_position($base);
-		var target_obj=getobj_position($target);
-		var sotai_x=moto_obj.x-target_obj.x;
-		var sotai_y=moto_obj.y-target_obj.y;
+function banner_move(banner,banner_parent,footer){
+	//設定
+	var $base=$('#sub_content');//位置固定バナー
+	var $target=$('#banner_group');//位置固定バナー親
+	var $footer=$('#footer');//フッター
+	var fotterY=getobj_position($footer).y;
+	var scrollObj=getScrollPosition();
+	var docObj=getDocumentSize();
+	var winObj=getScreenSize();
+	var baseObj=getobj_position($base);
+	var targetObj=getobj_position($target);
 
 
-		// console.log('on:'+-moto_obj.y);
-		// console.log("基準オブジェクトの位置x："+moto_obj.x+":y"+moto_obj.y);
-		// console.log("ターゲットの幅："+target_obj.w+":高さ"+target_obj.h);
-		// console.log("ターゲットの位置x："+target_obj.x+":y"+target_obj.y);
-		console.log("スクロールの位置x："+s_obj.x+":y"+s_obj.y);
-		console.log("ウィンドウのサイズ：x"+winObj.x+"y:"+winObj.y);
-		console.log("ドキュメントのサイズ："+d_obj.h+"footer位置:"+under_y);
+	// console.log('on:'+-baseObj.y);
+	// console.log("基準オブジェクトの位置x："+baseObj.x+":y"+baseObj.y);
+	// console.log("ターゲットの幅："+targetObj.w+":高さ"+targetObj.h);
+	// console.log("ターゲットの位置x："+targetObj.x+":y"+targetObj.y);
+	console.log("スクロールの位置x："+scrollObj.x+":y"+scrollObj.y);
+	console.log("ウィンドウのサイズ：x"+winObj.x+"y:"+winObj.y);
+	console.log("ドキュメントのサイズ："+docObj.h+"footer位置:"+fotterY);
 
-		if(winObj.x>768){
-			//console.log('on');
-			// console.log("場所０0");
-
-		if(moto_obj.y+target_obj.h>=winObj.y){
-
-
-		//bannerがwindow高さより低い場合
-		if(moto_obj.y>=s_obj.y){
-
-			$("#content").css('background-color',"#FFF");
-			// console.log("場所０１");
-			//posi_abs($target,0)
-		}else {
-			//console.log('off:'+-moto_obj.y);
-			$("#content").css('background-color',"#000");
-			//posi_fix($target,winObj.y-target_obj.h);
-			// console.log("場所０2");
-
-		}
+	if(winObj.x>768){
 
 		//bannerがwindow高さより高い場合
-		if(moto_obj.y+target_obj.h<=s_obj.y+winObj.y){
-			posi_fix($target,winObj.y-target_obj.h);
-			console.log(winObj.y-target_obj.h);
+		if(baseObj.y+targetObj.h>=winObj.y){
+		if(baseObj.y+targetObj.h<=scrollObj.y+winObj.y){
+			posi_fix($target,winObj.y-targetObj.h);
+			console.log(winObj.y-targetObj.h);
 			$("#main_content").css('background-color',"#000");
-			if(s_obj.y+winObj.y>=under_y){
-				posi_relative($target,under_y-target_obj.h-moto_obj.y);
+			if(scrollObj.y+winObj.y>=fotterY){
+				posi_relative($target,fotterY-targetObj.h-baseObj.y);
 			}
-
 		}else{
 			$("#main_content").css('background-color',"#F00");
 			posi_relative($target,0);
+			}
+			/*bannerの高さがウィンドウの高さ以上ここまで*/
+		}else{
+			//bannerがwindow高さより低い場合
+			if(baseObj.y<=scrollObj.y){
+				$("#content").css('background-color',"#FFF");
+				posi_fix($target,0)
+			}else {
+				$("#content").css('background-color',"#000");
+				posi_relative($target,0);
+			}
+			/*bannerの高さがウィンドウの高さ以下ここまで*/
 		}
-
-
-		//バナーの高さとウィンドウの高さの関係による？
-		//if(under_y<winObj.y+s_obj.y){
-			//if(s_obj.y>=under_y-target_obj.h){
-				//$("#content").css('background-color',"pink");
-				//posi_abs($target,under_y-target_obj.h-moto_obj.y);
-				//console.log("場所０3");
-			//}
-		//}
-
-		//$("#content").css('background-color',"#000");
-		//console.log("場所０4");
-		/*bannerの高さがウィンドウの高さ以上ここまで１*/}
-
-		}
-
-		//ページ下部固定
-
-		//確認
-		//console.log(sotai_x+":"+sotai_y+"スクロール"+s_obj.y+"underY"+under_y);
-
+	}else{
+	/*幅が768px以下*/
 	}
+
+
+}
